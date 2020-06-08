f106d4030d827f54622a9ea82c1f2227a63d7f87
casey <coconnor@redhat.com>
2020-06-08 13:36:26 -0600

    Appends df to minBars, needs multi-symbol functionality still

--- streamData.py
+++ streamData.py
@@ -6,26 +6,14 @@ from update_assets import update_assets
 from warn import warn
 from time import sleep
 
-#nest_asyncio.apply()
+
 update_assets(Algo)
-
 symbols = list(Algo.assets.keys())[:10]
-logger = logging.getLogger()
 
-fmt = '%(asctime)s:%(filename)s:%(lineno)d:%(levelname)s:%(name)s:%(message)s'
-logging.basicConfig(level=logging.INFO, format=fmt)
-fh = logging.FileHandler('console.log')
-fh.setLevel(logging.INFO)
-fh.setFormatter(logging.Formatter(fmt))
-logger.addHandler(fh)
 
-symbols = list(Algo.assets.keys())[:10]
-# 	print(Algo.minBars)
-# 	print("Tracking {} symbols.".format(len(symbols)))
-
-@conn.on(r'^account_updates$')
+@conn.on(r'account_updates')
 async def on_account_updates(conn, channel, account):
-	logger.info(f'account_updates {account}')
+	print(f'{alpacaPaper.get_account()}')
 	#symbol = data.order['symbol']
 	
 # @conn.on(r'^A$')
@@ -34,55 +22,55 @@ async def on_account_updates(conn, channel, account):
 # 	#symbol = data.symbol
 # 	#print(symbol)
 
-@conn.on(r'^AM$')
+@conn.on(r'AM')
 async def on_minute(conn, channel, data):
-	if data.symbol in symbols:
-		save_bars('minBars', data)
+	#logger.info(f'{data}')
+	print(f'{data.symbol} Data: \n {data}')
+	#if data.symbol in symbols:
+	save_bars('minBars', data)
 
 
 def save_bars(barType, data):
 	# barType: 'secBars' or 'minBars'
 	# data: raw stream data
-
-	# start writing
-	Algo.writing = True
-	print(Algo.writing)
-	# copy bars to Algo.assets
-	Algo.minBars.append(pd.DataFrame({
+	df = pd.DataFrame({
             'open': data.open,
             'high': data.high,
             'low': data.low,
             'close': data.close,
             'volume': data.volume,
-        }, index=[data.start]))
-	
-	bars = Algo.__getattribute__(barType)
-	for bar in bars:
-		assets = Algo.assets[bar.symbol][barType] 
-		assets = assets.append(bar.df)
-	logger.getChild(symbol).info(f'received bar start = {bar.start}, close = {bar.close}, len(bars) = {len(symbols)}')
-	# Done writing
-	Algo.writing = False
+        }, index=[data.start])
+	# copy bars to Algo.assets
+	Algo.minBars.append(df)
+	print(f'Saving bar for {data.symbol}')
+	print(f'{df}')
+	print(f'-------------------------------------------------------------')
+	print(f'{Algo.minBars}')
+	# for bar in bars:
+	# 	assets = Algo.assets[bar.symbol][barType] 
+	# 	assets = assets.append(bar.df)
+	# logger.info(f'received bar start = {bar.start}, close = {bar.close}, len(bars) = {len(symbols)}')
+
+
 
 
 print("Tracking {} symbols.".format(len(symbols)))
-channels = ['trade_updates','AM.*']
+channels = ['trade_updates','AM.SPY']
 
 # Add symbols to minute data (AM) and second data (A) 
-for symbol in symbols:
-	symbol_channels = ['AM.{}'.format(symbol)]
-	#symbol_channels = ['A.{}'.format(symbol) , 'AM.{}'.format(symbol)]
-	channels += symbol_channels
+# for symbol in symbols:
+# 	symbol_channels = ['AM.{}'.format(symbol)]
+# 	#symbol_channels = ['A.{}'.format(symbol) , 'AM.{}'.format(symbol)]
+# 	channels += symbol_channels
 
 # Defining the loop for the threads
 async def periodic():
 	while True:
 		if not alpacaPaper.get_clock().is_open:
-			logger.info('exit as market is not open')
+			print(f'Market is not open.')
 			sys.exit(0)
-		print(alpacaPaper.get_account())
-		print(channels)
-		await asyncio.sleep(30)
+		#print(alpacaPaper.get_account())
+		await asyncio.sleep(10)
 
 
 loop = conn.loop
