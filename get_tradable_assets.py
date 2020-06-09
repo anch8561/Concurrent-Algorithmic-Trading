945614cb0f76f065d30da4aaa3cb0686a7d732f2
Andrew Charters <anch8561@colorado.edu>
2020-06-09 16:37:24 -0600

    updated get_tradable_assets

--- get_tradable_assets.py
+++ get_tradable_assets.py
@@ -17,12 +17,14 @@ def get_tradable_assets(algos, debugging=False):
     # check for first run
     logging = False if Algo.assets == {} else True
 
-    # get activeSymbols
+    # get alpaca assets and polygon tickers
     alpacaAssets = alpaca.list_assets('active', 'us_equity')
     if debugging: alpacaAssets = alpacaAssets[:100]
     polygonTickers = alpaca.polygon.all_tickers()
-    activeSymbols = []
+    
+    # get activeSymbols
     # NOTE: this takes a long time. Would it be faster with sort?
+    activeSymbols = []
     for ii, asset in enumerate(alpacaAssets):
         print(f'Checking asset {ii} / {len(alpacaAssets)}')
 
@@ -46,61 +48,49 @@ def get_tradable_assets(algos, debugging=False):
     
     # remove inactive assets
     for symbol in inactive:
+        remove_asset(symbol, algos)
         if logging: print(f'"{symbol}" is no longer active')
 
-        # remove from assets
-        Algo.assets.pop(symbol)
-
-        # check for positions
-        # TODO: check algos
-        for positions in (Algo.livePositions, Algo.paperPositions):
-            if symbol in positions:
-                position = positions[symbol]
-                warn(f'You have {position} shares in {symbol} (inactive)')
-                # TODO: how to handle? (inactive on alpaca, not marginable, below price threshold)
-
-        # check for orders
-        for algo in algos:
-            for ii, order in enumerate(algo.orders):
-                if order['symbol'] == symbol:
-                    algo.alpaca.cancel_order(order['id'])
-                    algo.orders.pop(ii)
-        for orders in (Algo.livePositions, Algo.paperOrders):
-            for ii, order in enumerate(orders):
-                if order['symbol'] == symbol:
-                    orders.pop(ii)
-
     # add tradable assets
+    for symbol in activeSymbols:
+        if symbol not in Algo.assets:
+            add_asset(symbol)
+            if logging: print(f'"{symbol}" is now active')
+
+    # set shortable flag
     for asset in alpacaAssets:
-        # get symbol and price (if on polygon)
-        symbol = asset.symbol
-        price = 0
-        for ticker in polygonTickers:
-            if ticker.ticker == symbol:
-                price = ticker.prevDay['l']
-                break
-
-        # check marginablility
-        # TODO: check leverage
-        if asset.marginable and price > 3:
-            # check for new asset
-            if symbol not in Algo.assets:
-                add_asset(symbol)
-                if logging: print(f'"{symbol}" is now active')
-
-            # set shortable flag
+        if asset in Algo.assets:
             Algo.assets[symbol]['shortable'] = asset.easy_to_borrow
             # TODO: warn about HTB positions
-            
-
-            # TODO: sector, industry, and metrics
+        
+        # TODO: sector, industry, and metrics
 
     # set lastSymbolUpdate
     Algo.lastSymbolUpdate = get_date()
 
 
-def remove_asset(symbol):
-    pass
+def remove_asset(symbol, algos):
+    # remove from assets
+    Algo.assets.pop(symbol)
+
+    # check for positions
+    # TODO: check algos
+    for positions in (Algo.livePositions, Algo.paperPositions):
+        if symbol in positions:
+            position = positions[symbol]
+            warn(f'You have {position} shares in {symbol} (inactive)')
+            # TODO: how to handle? (inactive on alpaca, not marginable, below price threshold)
+
+    # check for orders
+    for algo in algos:
+        for ii, order in enumerate(algo.orders):
+            if order['symbol'] == symbol:
+                algo.alpaca.cancel_order(order['id'])
+                algo.orders.pop(ii)
+    for orders in (Algo.livePositions, Algo.paperOrders):
+        for ii, order in enumerate(orders):
+            if order['symbol'] == symbol:
+                orders.pop(ii)
 
 def add_asset(symbol):
     # add key
