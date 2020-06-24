73e6228f6f1283c3e95245b37344a46c0e8dad36
Andrew Charters <anch8561@colorado.edu>
2020-06-23 23:32:01 -0600

    conditions WIP

--- indicators.py
+++ indicators.py
@@ -10,18 +10,18 @@ from warn import warn
 class Indicator:
     def __init__(self, func, style, numBars, barType, **kwargs):
         self.func = func
-        self.style = style
-        if self.style == 'rank':
+        self.style = style # 'val' or 'rank'
+        if self.style == 'rank': # val indicator must be ticked first
             self.valName = Indicator(self.func, 'val', numBars, barType).name
         self.numBars = numBars
         self.barType = barType
         self.name = func.__name__ + str(style).capitalize() + str(numBars) + barType.capitalize()
-        for key, val in kwargs.items():
+        for key, val in kwargs.items(): # e.g. moving average function
             self.__setattr__(key, val)
             self.name += str(val).capitalize()
         
     def tick(self):
-        if self.style == 'bool' or 'val':
+        if self.style == 'val':
             for asset in Algo.assets.values():
                 val = self.func(asset)
                 try: asset[self.name].append(val)
@@ -42,14 +42,9 @@ def momentum(self, asset):
 
 def volume(self, asset):
     volume = 0
-    for day in range(self.days):
-        volume += asset['dayBars'].iloc[-day].volume
+    for bar in range(self.numBars):
+        volume += asset[self.barType].iloc[-bar].volume
     asset[self.name] = volume
-
-# bool functions
-def positiveMomentum(self, asset):
-    valName = Indicator(momentum, 'val', self.numBars, self.barType).name
-    return asset[valName] > 0
 
 # instances
 indicators = []
@@ -57,5 +52,20 @@ for barType in ('secBars', 'minBars', 'dayBars'):
     for numBars in (1, 2, 3, 5, 10, 20):
         indicators.append(Indicator(momentum, 'val', numBars, barType))
         indicators.append(Indicator(momentum, 'rank', numBars, barType))
+        # indicators.append(Indicator(momentum, '>', numBars, barType, val=0))
         indicators.append(Indicator(volume, 'val', numBars, barType))
-        indicators.append(Indicator(positiveMomentum, 'bool', numBars, barType))
+
+
+# class Condition:
+#     def __init__(self, left, comparitor, right):
+#         self.left = left
+#         self.comparitor = comparitor
+#         self.right = right
+    
+#     def check(self):
+#         if self.comparitor == '<': return self.left() < self.right()
+
+# example
+algo.enterConditions = [
+    lambda asset: asset['momentumVal5Day'] > asset['momentumVal1Day']
+]
