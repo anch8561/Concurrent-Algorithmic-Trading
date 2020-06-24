64bf8bb6414ae73160c1bd76e5b58ff83bcd0bfc
Andrew Charters <anch8561@colorado.edu>
2020-06-24 11:46:39 -0600

    tabling indicators

--- indicators.py
+++ indicators.py
@@ -3,72 +3,82 @@
 
 from Algo import Algo
 from warn import warn
+import statistics as stats
 
 # TODO: confirm indices have correct timestamps
 
-# class
+# classes
 class Indicator:
     def __init__(self, func, style, numBars, barType, **kwargs):
         self.func = func
-        self.style = style # 'val' or 'rank'
-        if self.style == 'rank': # val indicator must be ticked first
-            self.valName = Indicator(self.func, 'val', numBars, barType).name
-        self.numBars = numBars
+        self.numBars = numBars # int
         self.barType = barType
-        self.name = func.__name__ + str(style).capitalize() + str(numBars) + barType.capitalize()
+        self.name = str(numBars) + barType.capitalize()
         for key, val in kwargs.items(): # e.g. moving average function
             self.__setattr__(key, val)
             self.name += str(val).capitalize()
-        
-    def tick(self):
-        if self.style == 'val':
-            for asset in Algo.assets.values():
-                val = self.func(asset)
-                try: asset[self.name].append(val)
-                except: asset[self.name] = [val]
-        elif self.style == 'rank':
-            assets = sorted(Algo.assets, key=lambda asset: asset[self.valName][-1])
-            for rank, asset in enumerate(assets):
-                try: asset[self.name].append(rank)
-                except: asset[self.name] = [rank]
-        else:
-            warn(f'unkown indicator style "{self.style}"')
+        self.name += func.__name__
 
-# val functions
+class ValIndicator(Indicator):
+    def __init__(self, func, numBars, barType, **kwargs):
+        super.__init__(func, numBars, barType, kwargs)
+        self.name += 'Val'
+
+    def tick(self):
+        for asset in Algo.assets.values():
+            val = self.func(asset)
+            try: asset[self.name].append(val)
+            except: asset[self.name] = [val]
+
+class RankIndicator(Indicator):
+    def __init__(self, func, numBars, barType, **kwargs):
+        super.__init__(func, numBars, barType, kwargs)
+        self.valName = Indicator(self.func, 'val', numBars, barType).name
+        self.name += 'Rank'
+
+    def tick(self):
+        assets = sorted(Algo.assets, key=lambda asset: asset[self.valName][-1])
+        for rank, asset in enumerate(assets):
+            try: asset[self.name].append(rank)
+            except: asset[self.name] = [rank]
+
+# functions
 def momentum(self, asset):
     openPrice = asset[self.barType].iloc[-self.numBars].open
     closePrice = asset[self.barType].iloc[-1].close
     return (closePrice - openPrice) / openPrice
 
-def volume(self, asset):
+def volume(self, asset, numBars=None, barType=None):
+    if numBars == None: numBars = self.numBars
+    if barType == None: barType = self.barType
     volume = 0
-    for bar in range(self.numBars):
-        volume += asset[self.barType].iloc[-bar].volume
-    asset[self.name] = volume
+    for bar in range(numBars):
+        volume += asset[barType].iloc[-bar].volume
+    return volume
+
+def volume_stdev(self, asset):
+    volumes = asset[self.barType].iloc[-self.numBars:].volume
+    return stats.stdev(volumes)
+
+def volume_num_stdevs(self, asset):
+    return volume(None, asset, 1, 'dayBars') / volume_stdev(self, asset)
+
+def overnight_hold(self, asset):
+    return momentum(self, asset) * volume_num_stdevs(self, asset)
 
 # instances
 indicators = []
 for barType in ('secBars', 'minBars', 'dayBars'):
     for numBars in (1, 2, 3, 5, 10, 20):
-        indicators.append(Indicator(momentum, 'val', numBars, barType))
-        indicators.append(Indicator(momentum, 'rank', numBars, barType))
-        # indicators.append(Indicator(momentum, '>', numBars, barType, val=0))
-        indicators.append(Indicator(volume, 'val', numBars, barType))
-
-# class Condition:
-#     def __init__(self, left, comparitor, right):
-#         self.left = left
-#         self.comparitor = comparitor
-#         self.right = right
-    
-#     def check(self):
-#         if self.comparitor == '<': return self.left() < self.right()
+        indicators.append(ValIndicator(momentum, numBars, barType))
+        indicators.append(ValIndicator(overnight_hold, numBars, barType))
+        indicators.append(RankIndicator(overnight_hold, numBars, barType))
 
 # example
 # conditions compare indicator values or ranks against other indicator values or ranks, algo variables, or constants
 # it may need access to algo scope to get variables such as rank limits (e.g. top 25 if that's how many positions we can afford to enter)
 # is there some other way to pass in extra variables? (while keeping it modular)
-algo.enterConditions = [
+enterConditions = [
     lambda asset: asset['momentumVal5Day'] > asset['momentumVal1Day'],
     lambda asset: asset['momentumVal5Day'] > 0,
     lambda self, asset: asset['momentumVal5day'] < self.momentumLimit, # this has same args as val functions above...
