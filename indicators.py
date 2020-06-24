51259d8ed026c03bf4ea88336e12498cba8827cf
Andrew Charters <anch8561@colorado.edu>
2020-06-24 00:00:15 -0600

    added examples and thoughts

--- indicators.py
+++ indicators.py
@@ -55,7 +55,6 @@ for barType in ('secBars', 'minBars', 'dayBars'):
         # indicators.append(Indicator(momentum, '>', numBars, barType, val=0))
         indicators.append(Indicator(volume, 'val', numBars, barType))
 
-
 # class Condition:
 #     def __init__(self, left, comparitor, right):
 #         self.left = left
@@ -66,6 +65,12 @@ for barType in ('secBars', 'minBars', 'dayBars'):
 #         if self.comparitor == '<': return self.left() < self.right()
 
 # example
+# conditions compare indicator values or ranks against other indicator values or ranks, algo variables, or constants
+# it may need access to algo scope to get variables such as rank limits (e.g. top 25 if that's how many positions we can afford to enter)
+# is there some other way to pass in extra variables? (while keeping it modular)
 algo.enterConditions = [
-    lambda asset: asset['momentumVal5Day'] > asset['momentumVal1Day']
+    lambda asset: asset['momentumVal5Day'] > asset['momentumVal1Day'],
+    lambda asset: asset['momentumVal5Day'] > 0,
+    lambda self, asset: asset['momentumVal5day'] < self.momentumLimit, # this has same args as val functions above...
+    lambda self, asset: asset['momentumRank5day'] <= self.numTradesToPlace # but self refers to algo, not indicator
 ]
