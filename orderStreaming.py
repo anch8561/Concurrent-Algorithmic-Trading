cbae4cf78bd4757c62b72c292c7e8e172aa5504a
casey <coconnor@redhat.com>
2020-06-25 14:04:01 -0600

    Fixed syntax issues

--- orderStreaming.py
+++ orderStreaming.py
@@ -1,19 +1,19 @@
 import asyncio, sys
 import pandas as pd
-from Algo import Algo
+from algoClasses import Algo
 from alpacaAPI import conn, connPaper
-from get_tradable_assets import get_tradable_assets
+from update_tradable_assets import update_tradable_assets
 from marketHours import get_time, get_open_time, get_close_time
 from warn import warn
 
 def stream():
    
-   @conn.on(r'trade_update')
+    @conn.on(r'trade_update')
     async def handle_trade_update(conn, channel, data):
         event = data.event
         id = data.order.id
     
-        try id in Algo.liveOrders[orderID] or id in Algo.paperOrders[orderID]:
+        try:
             #set orders and positions to live or paper
             if id in Algo.liveOrders:
                 orders = Algo.liveOrders
@@ -40,36 +40,26 @@ def stream():
             print(f'Invalid order id: {id}')
 
 
-    # async def periodic():
-    #     while True:
-    #         if (
-    #             get_time() < get_open_time() or
-    #             get_time() > get_close_time()
-    #         ):
-    #             print(f'Market is closed')
-    #             sys.exit(0) # FIX: closes program
-    #         else:
-    #             loop.stop
-    #             #await asyncio.sleep(5)
+    async def periodic():
+        while True:
+            if (
+                get_time() < get_open_time() or
+                get_time() > get_close_time()
+            ):
+                print(f'Market is closed')
+                sys.exit(0) # FIX: closes program
+            else:
+                loop.stop
+                #await asyncio.sleep(5)
 
     channels = ['trade_update']
-    run_ws(conn, channels)
 
 
-# Handle failed websocket connections by reconnecting
-def run_ws(conn, channels):
-    try:
-        conn.run(channels)
-    except Exception as e:
-        print(e)
-        conn.close()
-        run_ws(conn, channels)
-
-    # loop = conn.loop
-    # loop.run_until_complete(asyncio.gather(
-    #     conn.subscribe(channels),
-    #     periodic()
-    # ))
-    # loop.close()
+    loop = conn.loop
+    loop.run_until_complete(asyncio.gather(
+        conn.subscribe(channels),
+        periodic()
+    ))
+    loop.close()
 
 stream()
