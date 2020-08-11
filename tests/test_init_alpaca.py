c5d56704493901e3c9d433a6d58108aefb551d88
Andrew Charters <anch8561@colorado.edu>
2020-08-11 10:17:25 -0600

    first unit test

---
+++ tests/test_init_alpaca.py
@@ -0,0 +1,17 @@
+import credentials
+import globalVariables as g
+from init_alpaca import init_alpaca
+
+import alpaca_trade_api as tradeapi
+
+def test_init_alpaca():
+    # get test account id
+    init_alpaca('dev')
+    testID = g.alpacaLive.get_account().id
+
+    # get real account id
+    creds = getattr(credentials, 'dev')
+    realID = tradeapi.REST(*creds.live).get_account().id
+    assert(testID == realID)
+
+    # NOTE: does not test polygon access or StreamConn
