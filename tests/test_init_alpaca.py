83ce8c8f5552cceed75163bd98e4824f88674ffd
Andrew Charters <anch8561@colorado.edu>
2020-08-30 20:37:38 -0600

    removed alpaca dependence

--- tests/test_init_alpaca.py
+++ tests/test_init_alpaca.py
@@ -3,17 +3,30 @@ import globalVariables as g
 from init_alpaca import init_alpaca
 
 import alpaca_trade_api as tradeapi
+from unittest.mock import Mock, call
 
 def test_init_alpaca():
-    # get test account id
+    # setup
+    class fakeCreds:
+        live = ('abc', '123', 'web.site')
+        paper = ('xyz', '890', 'site.web')
+    credentials.dev = fakeCreds
+    credentials.test = fakeCreds
+    credentials.prod = fakeCreds
+
+    class alpaca:
+        polygon = 1234
+    tradeapi.REST = Mock(return_value=alpaca)
+    tradeapi.StreamConn = Mock()
+
+    # test
     init_alpaca('dev')
-    testID = g.alpacaLive.get_account().id
-
-    # get real account id
-    creds = getattr(credentials, 'dev')
-    realID = tradeapi.REST(*creds.live).get_account().id
-
-    # compare id
-    assert testID == realID
-
-    # NOTE: does not test polygon access or StreamConn
+    calls = [
+        call(*credentials.dev.live),
+        call(*credentials.dev.paper),
+        call(*credentials.prod.live)]
+    tradeapi.REST.assert_has_calls(calls)
+    calls = [
+        call(*credentials.dev.live, data_stream='polygon'),
+        call(*credentials.dev.paper, data_stream='polygon')]
+    tradeapi.StreamConn.assert_has_calls(calls)
