867b79904600524f9af471989cc86cfefae5fd7d
Andrew Charters <anch8561@colorado.edu>
2020-09-26 13:38:13 -0600

    fixed is_market_day and reorganized

--- timing.py
+++ timing.py
@@ -18,10 +18,6 @@ def get_time():
     # returns: current nyc datetime
     return datetime.now(g.nyc)
 
-def get_time_str():
-    # returns: nyc time str; e.g. '08:35:41.736216' (HH:MM:SS.US)
-    return get_time().strftime('%H:%M:%S.%f')
-
 def get_market_open():
     # returns: nyc market open datetime
     return datetime.combine(
@@ -36,17 +32,20 @@ def get_market_close():
         time = calendar[i_today].close,
         tzinfo = get_time().tzinfo)
 
+def update_time():
+    g.now = get_time()
+    g.TTOpen = get_market_open() - g.now
+    g.TTClose = get_market_close() - g.now
+
+def get_time_str():
+    # returns: nyc time str; e.g. '08:35:41.736216' (HH:MM:SS.US)
+    return get_time().strftime('%H:%M:%S.%f')
+
 def get_date(offset=0):
     # offset: int, days relative to today (-1 is yesterday)
     # returns: nyc date str; e.g. '2020-05-28' (YYYY-MM-DD)
-    date = datetime.now(g.nyc) + timedelta(offset)
+    date = get_time() + timedelta(offset)
     return date.strftime('%Y-%m-%d')
-
-def get_market_date(offset=0):
-    # offset: int, market days relative to today (-1 is prev market day)
-    # returns: nyc date str; e.g. '2020-05-28' (YYYY-MM-DD)
-    if offset > 0 and not is_market_day(): offset -= 1
-    return calendar[i_today + offset].date.strftime('%Y-%m-%d')
 
 def is_market_day(offset=0):
     # offset: int, days relative to today (-1 is yesterday)
@@ -64,6 +63,7 @@ def is_market_day(offset=0):
 
     # future day
     if offset > 0:
+        if not is_market_day(): offset -= 1
         for ii in range(1, offset+1, 1):
             calDateStr = calendar[i_today + ii]._raw['date']
             if dateStr == calDateStr: return True
@@ -72,7 +72,8 @@ def is_market_day(offset=0):
     # today
     return dateStr == calendar[i_today]._raw['date']
 
-def update_time():
-    g.now = get_time()
-    g.TTOpen = get_market_open() - g.now
-    g.TTClose = get_market_close() - g.now
+def get_market_date(offset=0):
+    # offset: int, market days relative to today (-1 is prev market day)
+    # returns: nyc date str; e.g. '2020-05-28' (YYYY-MM-DD)
+    if offset > 0 and not is_market_day(): offset -= 1
+    return calendar[i_today + offset].date.strftime('%Y-%m-%d')
