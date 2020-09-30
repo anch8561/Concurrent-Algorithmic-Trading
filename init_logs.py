79fe97798b9235821d0b79140dda3ae119f5bdd5
Andrew Charters <anch8561@colorado.edu>
2020-09-30 15:59:12 -0600

    display full dataframes

--- init_logs.py
+++ init_logs.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 import logging, logging.handlers
 from os import mkdir
+import pandas as pd
 from pytz import timezone
 
 def init_formatter():
@@ -32,6 +33,9 @@ def init_primary_logs(logLevel, env, fmtr):
     # create logPath if needed
     try: mkdir(c.logPath)
     except Exception: pass
+
+    # display full dataframes
+    pd.set_option("display.max_rows", None, "display.max_columns", None)
 
     # handlers
     consoleHdlr = logging.StreamHandler()
