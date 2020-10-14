e10fd7057b82168d9578a5e7c45c0a99e2fe924b
Andrew Charters <anch8561@colorado.edu>
2020-10-13 21:42:09 -0600

    get_historic_bars

--- backtesting.py
+++ backtesting.py
@@ -6,10 +6,29 @@ import timing
 import alpaca_trade_api, sys
 from argparse import ArgumentParser
 from datetime import datetime
+from pandas import DataFrame
+from pytz import timezone
 from unittest.mock import patch
 
-# pick dates or type (bull, bear, volatile, etc)
+# test individual algos and/or whole system
+
 # record performance each day & stats about day (summary)
+
+# events (https://en.wikipedia.org/wiki/List_of_stock_market_crashes_and_bear_markets)
+
+# detect on major indices (and/or individual stocks)
+# bull / bear (MAX), volatile / stagnant (?), crash (?), black swan (daily high-low spread)
+
+# bear markets
+# 2007-10-11 to 2009-03
+# 2008-09-16 to ?
+# 2011-05-02 to 2011-10-04
+# 2015-08-18 to 2015-08-21
+# 2018-09-20 to 2018-12-24
+# 2020-02-24 to 2020-03-23
+
+# black swan
+# 2010-05-06
 
 if __name__ == '__main__':
     # parse arguments
@@ -33,20 +52,40 @@ def parse_args(args):
     parser.add_argument(
         '--dates',
         nargs = 2,
-        help = '') # max range?
+        help = 'e.g. 2004-01-01 (earliest date)')
     parser.add_argument(
         '--market',
-        choices = ['bull', 'bear', 'volatile']
-        help = '')
+        choices = ['bull', 'bear', 'volatile', 'stagnant', 'rally', 'crash', 'black swan'],
+        help = 'bull: 10 day SMA > 20 day SMA\n' + \
+            'bear: 10 day SMA < 20 day SMA\n' + \
+            'volatile: weeks with stdev (from 5 day SMA) > 1%\n' + \
+            'stagnant: weeks with stdev (from 5 day SMA) < 1%\n' + \
+            'rally: weeks with gains over 5%\n' + \
+            'crash: weeks with drops over 5%\n' + \
+            'black swan: days with deltas over 5%\n') # estimates subject to change
     return parser.parse_args(args)
 
+
+
+
 def get_historic_bars(symbols, fromDate, toDate):
-    for symbol in symbols:
-        realAlpaca.polygon.historic_agg_v2(symbol, 1, 'minute', fromDate, toDate)
-        # 3000 minutes
-        # 1 day = 400 minutes
-        # 1 week = 2000 minues
-        # extended hours?
+    # symbols: list of str
+    # fromDate: str; e.g. '2004-01-01'
+    # toDate: str; e.g. 2020-01-01'
+
+    fromDate = g.nyc.localize(datetime.strptime(fromDate, '%Y-%m-%d'))
+    toDate = g.nyc.localize(datetime.strptime(toDate, '%Y-%m-%d'))
+
+    bars = {}
+    while fromDate < toDate:
+        for symbol in symbols:
+            bars[symbol] = DataFrame()
+            newBars = alpaca.polygon.historic_agg_v2('AAPL', 1, 'minute', fromDate, toDate)
+            newBars = newBars.df.iloc[:5000] # remove extra toDate data
+            bars[symbol] = bars[symbol].append(newBars).drop_duplicates()
+            # TODO: drop extended hours
+        fromDate = bars[symbols[0]].index[-1]
+    # bars.to_csv('test.txt')
 
 def get_next_minute_bars(): pass
 
