83797cb05655d51dafc340fd23f101d8a5a2ac56
Andrew Charters <anch8561@colorado.edu>
2020-11-17 15:02:22 -0700

    combined metrics

--- backtesting/results.py
+++ backtesting/results.py
@@ -7,9 +7,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import ta
 from datetime import datetime, timedelta
+from logging import getLogger
 from pytz import timezone
 from unittest.mock import patch
 
+log = getLogger()
 nyc = timezone('America/New_York')
 
 def get_dates(dates):
@@ -17,25 +19,63 @@ def get_dates(dates):
     toDate = nyc.localize(datetime.strptime(dates[1], '%Y-%m-%d')) + timedelta(1)
     return fromDate, toDate
 
+def get_combined_metrics(algos: list, dates: list) -> dict:
+    # algos: algos to be combined
+    # dates: start and end dates (YYYY-MM-DD)
+    # returns: summary of daily performance of combined algos; {mean, stdev, min, max}
 
-def get_metrics(dates):
-    # suppress SettingWithCopyWarning
-    pd.set_option('mode.chained_assignment', None)
+    try: # get growth
+        # FIX: night algos start and stop on different days
+        df = pd.DataFrame()
+        for algo in algos:
+            history = sorted(algo.history, reverse=True)
+            for date in history:
+                if dates[0] <= date and date <= dates[1]:
+                    growth = 0
+                    startEquity = 0
+                    for entry in algo.history[date].values():
+                        if entry['event'] == 'start':
+                            startEquity = entry['equity']
+                        elif entry['event'] == 'stop' and startEquity:
+                            stopEquity = entry['equity']
+                            try:
+                                growth = (1 + growth) * stopEquity / startEquity - 1
+                            except Exception as e:
+                                if startEquity != 0: log.exception(e)
+                            startEquity = 0
+                    df.loc[date, algo.name] = growth
+    except Exception as e: log.exception(e)
+    
+    df['combined'] = df.sum(1, min_count=len(algos))
+    metrics = {}
+    metrics['mean'] = df.combined.mean()
+    metrics['stdev'] = df.combined.std()
+    metrics['min'] = df.combined.min()
+    metrics['max'] = df.combined.max()
+    
+    return metrics
 
-    # get numDays
-    fromDate, toDate = get_dates(dates)
-    numDays = (toDate - fromDate).days
-
+def get_metrics(dates, deltaNeutral=True):
     # get metrics
-    metrics = pd.DataFrame()
+    metrics = pd.DataFrame(columns=['name', 'mean', 'stdev', 'min', 'max'])
     with patch('algos.c', c), patch('algoClass.c', c):
         algos = init_algos(True, None)
-    for algo in algos['all']:
-        algoMetrics = algo.get_metrics(numDays)
-        algoMetrics['name'] = algo.name
+    if deltaNeutral:
+        for algo1 in algos['all']:
+            if algo1.name[-4:] == 'long':
+                name = algo1.name[:-4] + 'short'
+                for algo2 in algos['all']:
+                    if algo2.name == name:
+                        algoMetrics = get_combined_metrics([algo1, algo2], dates)
+                        algoMetrics['name'] = algo1.name[:-5]
+                        metrics = metrics.append(algoMetrics, ignore_index=True)
+                        break
+    else:
+        for algo in algos['all']:
+            algoMetrics = get_combined_metrics([algo], dates)
+            algoMetrics['name'] = algo.name
+            metrics = metrics.append(algoMetrics, ignore_index=True)
 
-        metrics = metrics.append(algoMetrics, ignore_index=True)
-    
     # exit
     pd.set_option("display.max_rows", None, "display.max_columns", None)
     return metrics
@@ -44,12 +84,15 @@ def get_metrics(dates):
 def plot_backtest(barFreq, symbol, dates, algoName):
     # NOTE: assumes only 1 backtest in logs
 
+    # suppress SettingWithCopyWarning
+    pd.set_option('mode.chained_assignment', None)
+
     # get price data
     bars = pd.read_csv(c.barPath + f'{barFreq}_{symbol}.csv',
         header=0, index_col=0, parse_dates=True)
     fromDate, toDate = get_dates(dates)
     data = bars.loc[fromDate:toDate]
-    data.index = data.index.tz_convert(nyc)
+    data.index = pd.DatetimeIndex(data.index).tz_convert(nyc)
     data['trades'] = 0
     data['tradePrice'] = 0
     
@@ -120,16 +163,22 @@ def plot_backtest(barFreq, symbol, dates, algoName):
         axs[0].bar(dayData.index[down], oc_height[down], 0.0005, oc_bottom[down],   color='r')
 
         # plot trades
-        dayData.tradePrice[dayData.trades == 2].plot(style='r^', markersize=9, markeredgecolor='k', ax=axs[0])
-        dayData.tradePrice[dayData.trades == 1].plot(style='k^', markersize=9, ax=axs[0])
-        dayData.tradePrice[dayData.trades == -1].plot(style='kv', markersize=9, ax=axs[0])
-        dayData.tradePrice[dayData.trades == -2].plot(style='gv', markersize=9, markeredgecolor='k', ax=axs[0])
+        try: dayData.tradePrice[dayData.trades == 2].plot(style='r^', markersize=9, markeredgecolor='k', ax=axs[0])
+        except: pass # plot fails if df is empty
+        try: dayData.tradePrice[dayData.trades == 1].plot(style='k^', markersize=9, ax=axs[0])
+        except: pass
+        try: dayData.tradePrice[dayData.trades == -1].plot(style='kv', markersize=9, ax=axs[0])
+        except: pass
+        try: dayData.tradePrice[dayData.trades == -2].plot(style='gv', markersize=9, markeredgecolor='k', ax=axs[0])
+        except: pass
 
         # plot volume
         axs[1].bar(dayData.index, dayData.volume, 0.0005)
 
     # exit
-    return figs, data
+    noTradesData = bars.loc[fromDate:toDate]
+    noTradesData.index = pd.DatetimeIndex(noTradesData.index).tz_convert(nyc)
+    return figs, data, noTradesData
 
 
 def plot_indicators(figs: list, data: pd.DataFrame, indicators: list):
