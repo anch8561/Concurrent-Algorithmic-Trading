13c62ede340a912ba56117f1b1e8625939a155c2
andrew <anch8561@colorado.edu>
2020-12-31 00:36:45 -0700

    read jsons instead of loading algos

--- backtesting/results.py
+++ backtesting/results.py
@@ -1,14 +1,35 @@
 import backtesting.config as c
 from algos import init_algos
 
-import os
+import json, os
 import pandas as pd
 from datetime import datetime, timedelta
+from typing import Dict, List, Literal, Union
 
-def get_combined_algo_history(algos: list, dates: list = []) -> pd.Series:
-    # algos: algos to be combined
-    # dates: start and stop date strings (YYYY-MM-DD)
-    # returns: combined growth fractions of algos with date str index
+def load_history(filePath: str):
+    with open(filePath, 'r') as f:
+        data = json.load(f)
+    return data['history']
+
+def get_combined_history(
+    histories: List[
+        Dict[
+            str, # date
+            Dict[
+                Literal['event', 'equity'],
+                Union[
+                    Literal['start', 'stop'], # event
+                    float]]]], # equity
+    dates: List[str] = []
+) -> pd.Series:
+    '''
+    histories: list of algo histories; [{date: {'event', 'equity'}}]
+        date: str; (YYYY-MM-DD)
+        'event': 'start' or 'stop'
+        'equity': float
+    dates: list; start and stop date strings (YYYY-MM-DD); empty means all dates
+    returns: pd.Series; combined growth fractions of algos with date str index
+    '''
 
     # check dates
     if dates == []: dates = ['0', '9']
@@ -16,50 +37,53 @@ def get_combined_algo_history(algos: list, dates: list = []) -> pd.Series:
     # get growth
     # FIX: night algos start and stop on different days
     df = pd.DataFrame()
-    for algo in algos:
-        history = sorted(algo.history)
-        for date in history:
+    for column, history in enumerate(histories):
+        dateKeys = sorted(history)
+        for date in dateKeys:
             if dates[0] <= date and date <= dates[1]:
                 growth = 0
                 startEquity = 0
-                for entry in algo.history[date].values():
+                for entry in history[date].values():
                     if entry['event'] == 'start':
                         startEquity = entry['equity']
                     elif entry['event'] == 'stop' and startEquity:
                         stopEquity = entry['equity']
                         growth = (1 + growth) * stopEquity / startEquity - 1
                         startEquity = 0
-                df.loc[date, algo.name] = growth
+                df.loc[date, column] = growth
             elif date > dates[1]: break
     
-    return df.sum(1, min_count=len(algos))
+    return df.sum(1, min_count=len(histories))
 
 def get_backtest_history(backtestDir: str, dates: list = [], deltaNeutral: bool = True) -> pd.DataFrame:
     # backtestDir: directory (within results path) where backtest is stored
     # dates: start and stop date strings (YYYY-MM-DD)
     # deltaNeutral: whether to combine long and short versions of algos
     # returns: growth fractions of algos with date str index
-
-    # init algos
+    
     algoPath = c.resultsPath + backtestDir + '/algos/'
-    logPath = c.resultsPath + backtestDir + '/logs/'
-    algos = init_algos(True, None, algoPath, logPath)
+    algoFileNames = os.listdir(algoPath)
     
     # get history
     history = pd.DataFrame()
     if deltaNeutral:
-        for algo1 in algos['all']:
-            if algo1.name[-5:] == '_long':
-                name = algo1.name[:-5]
-                for algo2 in algos['all']:
-                    if algo2.name == name + '_short':
-                        algoHistory = get_combined_algo_history([algo1, algo2], dates)
+        for fileName1 in algoFileNames:
+            if fileName1[-10:] == '_long.json':
+                name = fileName1[:-10]
+                for fileName2 in algoFileNames:
+                    if fileName2 == name + '_short.json':
+                        algoHistories = [ # load files
+                            load_history(algoPath + fileName1),
+                            load_history(algoPath + fileName2)]
+                        algoHistory = get_combined_history(algoHistories, dates) # combine and filter dates
                         history[name] = algoHistory
                         break
     else:
-        for algo in algos['all']:
-            algoHistory = get_combined_algo_history([algo], dates)
-            history[algo.name] = algoHistory
+        for fileName in algoFileNames:
+            algoHistories = [load_history(algoPath + fileName)] # load file
+            algoHistory = get_combined_history(algoHistories, dates) # filter dates
+            algoName = fileName[:-5]
+            history[algoName] = algoHistory
     return history
 
 def get_combined_backtest_history(backtestDir: str, dates: list = [], deltaNeutral: bool = True) -> pd.DataFrame:
@@ -68,12 +92,19 @@ def get_combined_backtest_history(backtestDir: str, dates: list = [], deltaNeutr
     # deltaNeutral: whether to combine long and short versions of algos
     # returns: growth fractions of algos with date str index
 
+    # append slash to backtestDir if needed
+    if backtestDir and backtestDir[-1] != '/':
+        backtestDir += '/'
+
+    # collect histories
     history = pd.DataFrame()
     backtestSubdirs = os.listdir(c.resultsPath + backtestDir)
     for backtestSubdir in backtestSubdirs:
-        path = backtestDir + '/' + backtestSubdir
-        backtestHistory = get_backtest_history(path, dates, deltaNeutral)
-        history = history.append(backtestHistory)
+        try: # expects all subdirectories to be backtests
+            path = backtestDir + backtestSubdir
+            backtestHistory = get_backtest_history(path, dates, deltaNeutral)
+            history = history.append(backtestHistory)
+        except Exception as e: print(e)
     return history
 
 def get_metrics(history: pd.DataFrame) -> pd.DataFrame:
